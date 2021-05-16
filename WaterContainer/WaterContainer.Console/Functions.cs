96e162ce13be7f0fd89dc1e2449aaa5a967dca8d
Rodrigo Saraya <linkin_rodx@hotmail.com>
2021-05-16 17:31:22 -0500

    first commit

---
+++ WaterContainer/WaterContainer.Console/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterContainer.Console
+{
+    public class Functions
+    {
+        public static int CalcMostWater(int[] array)
+        {
+            int container = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    var calc = Math.Min(array[i], array[j]) * (j - i);
+
+                    if (container < calc)
+                    {
+                        container = calc;
+                    }
+                }
+            }
+
+            return container;
+        }
+    }
+}
---
+++ WaterContainer/WaterContainer.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WaterContainer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            int[] array2 = new int[] { 1, 1 };
+            int[] array3 = new int[] { 4, 3, 2, 1, 4 };
+            int[] array4 = new int[] { 1, 2, 1 };
+
+            var test1 = Functions.CalcMostWater(array1);
+            var test2 = Functions.CalcMostWater(array2);
+            var test3 = Functions.CalcMostWater(array3);
+            var test4 = Functions.CalcMostWater(array4);
+        }
+    }
+}
